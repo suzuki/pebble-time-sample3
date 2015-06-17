17117d84c622aa095bb64af76c68844b102cbbff
Norio Suzuki <norio.suzuki@gmail.com>
2015-06-17 09:31:47 +0900

    Added animation sample.

--- src/pebble-time-sample3.c
+++ src/pebble-time-sample3.c
@@ -1,60 +1,102 @@
 #include <pebble.h>
 
-static Window *window;
-static TextLayer *text_layer;
+#define BOX_SIZE 20
+#define ANIM_DURATION 500
+#define ANIM_DELAY 300
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+static Window *s_main_window;
+static Layer *s_box_layer;
+static PropertyAnimation *s_box_animation;
+static int s_current_stage = 0;
+
+static void next_animation();
+
+static void anim_stopped_handler(Animation *animation, bool finished, void *context) {
+#ifdef PBL_PLATFORM_APLITE
+  property_animation_destroy(s_box_animation);
+#endif
+
+  if (finished) {
+    next_animation();
+  }
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+static void next_animation() {
+  GRect start, finish;
+  switch (s_current_stage) {
+  case 0:
+    start = GRect(0, 0, BOX_SIZE, BOX_SIZE);
+    finish = GRect(144 - BOX_SIZE, 0, BOX_SIZE, BOX_SIZE);
+    break;
+  case 1:
+    start = GRect(144 - BOX_SIZE, 0, BOX_SIZE, BOX_SIZE);
+    finish = GRect(144 - BOX_SIZE, 168 - BOX_SIZE, BOX_SIZE, BOX_SIZE);
+    break;
+  case 2:
+    start = GRect(144 - BOX_SIZE, 168 - BOX_SIZE, BOX_SIZE, BOX_SIZE);
+    finish = GRect(0, 168 - BOX_SIZE, BOX_SIZE, BOX_SIZE);
+    break;
+  case 3:
+    start = GRect(0, 168 - BOX_SIZE, BOX_SIZE, BOX_SIZE);
+    finish = GRect(0, 0, BOX_SIZE, BOX_SIZE);
+    break;
+  default:
+    start = GRect(0, 0, BOX_SIZE, BOX_SIZE);
+    finish = GRect(0, 0, BOX_SIZE, BOX_SIZE);
+  }
+
+  s_box_animation = property_animation_create_layer_frame(s_box_layer, &start, &finish);
+  animation_set_duration((Animation*)s_box_animation, ANIM_DURATION);
+  animation_set_delay((Animation*)s_box_animation, ANIM_DELAY);
+  animation_set_curve((Animation*)s_box_animation, AnimationCurveEaseInOut);
+  animation_set_handlers((Animation*)s_box_animation, (AnimationHandlers) {
+      .stopped = anim_stopped_handler
+  }, NULL);
+  animation_schedule((Animation*)s_box_animation);
+
+  s_current_stage = (s_current_stage + 1) % 4;
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+static void update_proc(Layer *layer, GContext *ctx) {
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, layer_get_bounds(layer), 4, GCornersAll);
 }
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  s_box_layer = layer_create(GRect(0, 0, BOX_SIZE, BOX_SIZE));
+  layer_set_update_proc(s_box_layer, update_proc);
+  layer_add_child(window_layer, s_box_layer);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  layer_destroy(s_box_layer);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
+  s_main_window = window_create();
+#ifdef PBL_SDK_2
+  window_set_fullscreen(s_main_window, true);
+#endif
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
-  const bool animated = true;
-  window_stack_push(window, animated);
+  window_stack_push(s_main_window, true);
+
+  next_animation();
 }
 
 static void deinit(void) {
-  window_destroy(window);
+  animation_unschedule_all();
+
+  window_destroy(s_main_window);
 }
 
 int main(void) {
   init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
   app_event_loop();
   deinit();
 }
